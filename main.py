516c98fe6bf433e085bb79e3b8bead389bc47fca
yunikeil <iyunakov@inbox.ru>
2023-10-31 01:54:02 +0300

    new routers added, docs now on /redirect, start from __main__

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+
 from database import SessionLocal, engine, Base
-from routers.user import router as UserRouter
+from routers import control_redirects, test_redirect
+
 
 Base.metadata.create_all(bind=engine)
-app = FastAPI()
-app.include_router(UserRouter)
+app = FastAPI(docs_url="/redirect/docs", redoc_url="/redirect/redocs")
+
+app.include_router(control_redirects.router)
+app.include_router(test_redirect.router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=2525, reload=True, workers=3)
+    uvicorn.run("main:app", host='127.0.0.1', port=2525, reload=True)
