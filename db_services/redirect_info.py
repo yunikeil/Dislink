270b254160fb02b3b837a6e904346846b61630b7
yunikeil <iyunakov@inbox.ru>
2023-10-31 01:55:53 +0300

    working beta version file

--- db_services/redirect_info.py
+++ db_services/redirect_info.py
@@ -1,3 +1,4 @@
+import sqlalchemy
 from sqlalchemy.orm import Session
 
 from db_models.redirect_info import RedirectInfo as RedirModelDB
@@ -5,18 +6,46 @@ from dto.redirect_info import RedirectInfo as RedirModelDTO
 
 
 def create_redirect(data: RedirModelDTO, db: Session):
-    redirect = RedirModelDB(server_link=data.server_link, domen_link=data.domen_link)
+    redirect = RedirModelDB(
+        server_id=data.server_id,
+        server_link=data.server_link,
+        domen_link=data.domen_link,
+    )
     try:
         db.add(redirect)
         db.commit()
         db.refresh(redirect)
-    except Exception as e:
-        print(e)
-    return redirect
+
+    except sqlalchemy.exc.IntegrityError as e:
+        error_messages = {
+            "UNIQUE constraint failed: RedirectInfo.server_id": f"server_id={data.server_id}({type(data.server_id)}, primary_key=True) already exists",
+            "UNIQUE constraint failed: RedirectInfo.server_link": f"server_link={data.server_link}({type(data.server_link)}, unique=True) already exists",
+            "UNIQUE constraint failed: RedirectInfo.domen_link": f"domen_link={data.domen_link}({type(data.domen_link)}, unique=True) already exists",
+        }
+        return { "error": error_messages.get(str(e.orig))}
+
+    return { "ok": redirect }
 
 
-def get_redirect(server_id: int, db: Session):
-    return db.query(RedirModelDB).filter(RedirModelDB.server_id == server_id).first()
+def get_redirect(db: Session, server_id: int = None, server_link: str = None, domen_link: str = None):
+    if server_id is not None:
+        return db.query(RedirModelDB).filter(RedirModelDB.server_id == server_id).first()
+    
+    if server_link is not None:
+        return (
+            db.query(RedirModelDB)
+            .filter(RedirModelDB.server_link == server_link)
+            .first()
+        )
+
+    if domen_link is not None:
+        return (
+            db.query(RedirModelDB)
+            .filter(RedirModelDB.domen_link == domen_link)
+            .first()
+        )
+    
+    raise "server_id must be int, not NoneType"    
 
 
 def update_redirect(server_id: int, data: RedirModelDTO, db: Session):
@@ -28,6 +57,7 @@ def update_redirect(server_id: int, data: RedirModelDTO, db: Session):
     db.add(redirect)
     db.commit()
     db.refresh(redirect)
+    
     return redirect
 
 
@@ -36,4 +66,5 @@ def remove_redirect(server_id: int, db: Session):
         db.query(RedirModelDB).filter(RedirModelDB.server_id == server_id).delete()
     )
     db.commit()
+
     return redirect
