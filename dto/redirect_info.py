6bb4159334d6a86a063658041a512d6bfbeb1b49
yunikeil <iyunakov@inbox.ru>
2023-10-31 01:56:07 +0300

    working beta version file

--- dto/redirect_info.py
+++ dto/redirect_info.py
@@ -2,5 +2,10 @@ from pydantic import BaseModel
 
 
 class RedirectInfo(BaseModel):
+    server_id: int
     server_link: str
     domen_link: str
+
+
+class RedirectError(BaseModel):
+    comment: str
