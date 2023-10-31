720a9d19ab68ec5f55d0c41885e21be11d38dfd1
yunikeil <iyunakov@inbox.ru>
2023-10-31 20:51:18 +0300

    main -> server rename

--- server.py
+++ server.py
@@ -12,4 +12,4 @@ app.include_router(control_redirects.router)
 app.include_router(test_redirect.router)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='127.0.0.1', port=2525, reload=True)
+    uvicorn.run("server:app", host='127.0.0.1', port=2525, reload=True)
