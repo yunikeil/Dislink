06c7f1bf20f14bd1dd38e4755f9f7e6f9c5a3997
yunikeil <iyunakov@inbox.ru>
2023-11-02 02:00:57 +0300

    discord.py working example

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import asyncio
+import logging
 
 import aeval
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 import configuration
@@ -52,15 +54,19 @@ class Bot(commands.Bot):
         self.OWNERS = configuration.bot_owners
         self.EVAL_OWNER = configuration.eval_owners
         self.cogs_add_on_ready = cogs_add_on_ready
+    
+    async def setup_hook(self):
+        if self.cogs_add_on_ready:
+            [
+                await self.load_extension(f"cogs.{cog}")
+                for cog in self.cogs_add_on_ready
+            ]
+        self.tree.copy_global_to(guild=discord.Object(id=1064192306904846377))
+        await self.tree.sync()
 
     async def on_ready(self):
         print(f"Logged in as {self.user} (ID: {self.user.id})\n------")
         if not self.DATA["bot-started"]:
-            if self.cogs_add_on_ready:
-                [
-                    await self.load_extension(f"cogs.{cog}")
-                    for cog in self.cogs_add_on_ready
-                ]
             application_info = await self.application_info()
             self.OWNERS.append(application_info.owner.id)
             self.EVAL_OWNER.append(application_info.owner.id)
--- cogs/redirect_manager.py
+++ cogs/redirect_manager.py
@@ -1,85 +1,30 @@
-import nextcord
 import logging
-from nextcord import Interaction, Locale
-from nextcord.ext import tasks, application_checks
-from nextcord.ext.commands import Bot, Cog
+
+import discord
+from discord import app_commands
+from discord.ext import commands, tasks
 
 
-logging.basicConfig(level=logging.INFO)
-
-
-class RedirManagerCog(Cog):
-    def __init__(self, bot: Bot):
+class MyCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.on_init.start()
 
     @tasks.loop(count=1)
     async def on_init(self):
-        await self.bot.sync_all_application_commands()
-
-    def cog_unload(self):
         pass
 
-    @nextcord.slash_command(
-        name="get_invite",
-        name_localizations={
-            Locale.en_US: "get_invite",
-            Locale.ru: "получить_приглашение",
-        },
-        description_localizations={
-            Locale.en_US: "Get an invite"
-        },
-        guild_ids=[1064192306904846377]
-    )
-    @application_checks.has_permissions(administrator=True)
-    async def get_invite(self, interaction: Interaction):
-        await interaction.response.send_message("123", ephemeral=True)
+    @app_commands.command(name="command-1")
+    async def my_command(self, interaction: discord.Interaction) -> None:
+        """ /command-1 """
+        await interaction.response.send_message("Hello from command 1!", ephemeral=True)
 
-    @nextcord.slash_command(
-        name="create_invite",
-        name_localizations={
-            Locale.en_US: "create_invite",
-            Locale.ru: "создать_приглашение",
-        },
-        description_localizations={
-            Locale.en_US: "Create an invite"
-        },
-        guild_ids=[1064192306904846377]
-    )
-    @application_checks.has_permissions(administrator=True)
-    async def create_invite(self, interaction: Interaction):
-        await interaction.response.send_message("123", ephemeral=True)
+    @app_commands.command(name="command-2")
+    @app_commands.guilds(1064192306904846377)
+    async def my_private_command(self, interaction: discord.Interaction) -> None:
+        """ /command-2 """
+        await interaction.response.send_message("Hello from private command!", ephemeral=True)
 
-    @nextcord.slash_command(
-        name="update_invite",
-        name_localizations={
-            Locale.en_US: "update_invite",
-            Locale.ru: "обновить_приглашение",
-        },
-        description_localizations={
-            Locale.en_US: "Update an invite",
-        },
-        guild_ids=[1064192306904846377]
-    )
-    @application_checks.has_permissions(administrator=True)
-    async def update_invite(self, interaction: Interaction):
-        await interaction.response.send_message("123", ephemeral=True)
-
-    @nextcord.slash_command(
-        name="delete_invite",
-        name_localizations={
-            Locale.en_US: "delete_invite",
-            Locale.ru: "удалить_приглашение",
-        },
-        description_localizations={
-            Locale.en_US: "Delete an invite",
-        },
-        guild_ids=[1064192306904846377]
-    )
-    @application_checks.has_permissions(administrator=True)
-    async def delete_invite(self, interaction: Interaction):
-        await interaction.response.send_message("123", ephemeral=True)
-
-def setup(bot: Bot):
-    print("RedirManagerCog loaded!")
-    bot.add_cog(RedirManagerCog(bot))
+async def setup(bot: commands.Bot) -> None:
+    logging.getLogger('discord.load.cogs').info("MyCog loaded!")
+    await bot.add_cog(MyCog(bot))
