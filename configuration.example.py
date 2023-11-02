dce31fd7e916cb9f381ca43910c6698965368ab0
yunikeil <iyunakov@inbox.ru>
2023-11-02 18:45:57 +0300

    config example added

---
+++ configuration.example.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+# Bot config
+
+bot_owners: List[int] = []
+eval_owners: List[int] = []
+
+cogs_on_start: List[str] = ["redirect_manager", "errors_handler"]
+
+client_id: int = ...
+permissions: int = 537184257
+invite_link: str = \
+    f"https://discord.com/api/oauth2/authorize?client_id={client_id}&permissions={permissions}&scope=bot%20applications.commands"
+
+discord_token: str = ""
+
+# Server config
+
+allowed_ips: List[str] = [] 
