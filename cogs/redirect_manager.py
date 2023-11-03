ddccdade546a2689dbc328a95e18c08ebb1f2670
yunikeil <iyunakov@inbox.ru>
2023-11-04 02:43:11 +0300

    RedirectCog frame*

--- cogs/redirect_manager.py
+++ cogs/redirect_manager.py
@@ -4,7 +4,8 @@ from discord import app_commands
 from discord.ext import commands, tasks
 from discord.app_commands import locale_str as _T
 
-class MyCog(commands.Cog):
+
+class RedirectCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         print(type(self.create_shortcut))
@@ -14,31 +15,39 @@ class MyCog(commands.Cog):
     async def on_init(self):
         pass
 
-    @app_commands.command(name=_T("create-shortcut"))
-    @app_commands.guilds(1064192306904846377)
+    @app_commands.command(
+        name=_T("create_shortcut_name"), description=_T("create_shortcut_description")
+    )
+    @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def create_shortcut(self, interaction: discord.Interaction) -> None:
-        # Здесь вы можете добавить код для создания сокращения
+
         await interaction.response.send_message("Shortcut created!", ephemeral=True)
 
-    @app_commands.command(name="get-shortcut")
-    @app_commands.guilds(1064192306904846377)
+    @app_commands.command(
+        name=_T("get_shortcut_name"), description=_T("create_shortcut_description")
+    )
+    @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def get_shortcut(self, interaction: discord.Interaction) -> None:
-        # Здесь вы можете добавить код для получения сокращения
+
         await interaction.response.send_message("Shortcut retrieved!", ephemeral=True)
 
-    @app_commands.command(name="update-shortcut")
-    @app_commands.guilds(1064192306904846377)
+    @app_commands.command(
+        name=_T("update_shortcut_name"), description=_T("create_shortcut_description")
+    )
+    @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def update_shortcut(self, interaction: discord.Interaction) -> None:
-        # Здесь вы можете добавить код для обновления сокращения
+
         await interaction.response.send_message("Shortcut updated!", ephemeral=True)
 
-    @app_commands.command(name="delete-shortcut")
-    @app_commands.guilds(1064192306904846377)
+    @app_commands.command(
+        name=_T("delete_shortcut_name"), description=_T("create_shortcut_description")
+    )
+    @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def delete_shortcut(self, interaction: discord.Interaction) -> None:
-        # Здесь вы можете добавить код для удаления сокращения
+
         await interaction.response.send_message("Shortcut deleted!", ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
-    logging.getLogger('discord.load.cogs').info("MyCog loaded!")
-    await bot.add_cog(MyCog(bot))
+    logging.getLogger("discord.load.cogs").info("RedirectCog loaded!")
+    await bot.add_cog(RedirectCog(bot))
