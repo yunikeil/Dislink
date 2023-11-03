c7a2e4c277bf54021d3d25c560d148fb42910a3d
yunikeil <iyunakov@inbox.ru>
2023-11-03 05:07:37 +0300

    router organization changes

--- routers/control_redirects.py
+++ routers/control_redirects.py
@@ -1,60 +1,54 @@
-from fastapi import APIRouter, Request, Depends, HTTPException, status
+from fastapi import APIRouter, Request, Depends, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from database import get_db
-
+from services.authentication import IpCheck
 from services import redirect_info as RedirectDB
 from dto import redirect_info as RedirictDTO
+import configuration
 
 
-router = APIRouter()
-ALLOWED_IPS = ["62.113.96.70"]
-
-
-def check_ip(request: Request):
-    client_ip = request.client.host
-    if client_ip not in ALLOWED_IPS:
-        raise HTTPException(status_code=404, detail="IP address is not allowed")
+checker = IpCheck(allowed_ips=configuration.control_redirects_allowed_ips)
+router = APIRouter(
+    prefix="/control",
+    tags=["redirect"],
+    dependencies=[Depends(checker.is_ip_allowed)],
+    responses={403: {"model": RedirictDTO.RedirectError}},
+)
 
 
 @router.post(
-    "/control/redirect",
-    tags=["control"],
+    "/redirect",
     responses={
         200: {"model": RedirictDTO.RedirectInfo},
-        400: {"model": RedirictDTO.RedirectError},
     },
 )
 async def create_redirect(
     request: Request,
     data: RedirictDTO.RedirectInfo,
     db: Session = Depends(get_db),
-):
-    check_ip(request)
+):      
     request.client.host
     result = RedirectDB.create_redirect(data, db)
     if text := result.get("error"):
         return JSONResponse({"comment": text}, status.HTTP_400_BAD_REQUEST)
-    
+
     return result.get("ok")
 
 
 @router.get(
-    "/control/redirect/{id}",
-    tags=["control"],
+    "/redirect/{id}",
     responses={200: {"model": RedirictDTO.RedirectInfo}},
 )
 async def get_redirect(request: Request, id: int, db: Session = Depends(get_db)):
-    check_ip(request)
     result = RedirectDB.get_redirect(db, server_id=id)
 
     return result
 
 
 @router.put(
-    "/control/redirect/{id}",
-    tags=["control"],
+    "/redirect/{id}",
     responses={200: {"model": RedirictDTO.RedirectInfo}},
 )
 async def update_redirect(
@@ -63,18 +57,14 @@ async def update_redirect(
     data: RedirictDTO.RedirectInfo,
     db: Session = Depends(get_db),
 ):
-    check_ip(request)
     result = RedirectDB.update_redirect(id, data, db)
-
     return result
 
 
 @router.delete(
-    "/control/redirect/{id}",
-    tags=["control"],
+    "/redirect/{id}",
     responses={200: {"model": bool}},
 )
 async def remove_redirect(request: Request, id: int, db: Session = Depends(get_db)):
-    check_ip(request)
     result = RedirectDB.remove_redirect(id, db)
     return bool(result)
