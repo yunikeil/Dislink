86d52f9a61c2ff0f395c5d0a0468482110f54826
yunikeil <iyunakov@inbox.ru>
2023-11-04 02:38:46 +0300

    changes in including router, tags -> _locales

--- server.py
+++ server.py
@@ -3,22 +3,23 @@ import json
 from fastapi import FastAPI
 
 from database import engine, Base
-from routers import control_redirects, test_redirect
+from routers import control_redirects, root
 
 
+routers = [control_redirects, root]
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
     version="0.1.0",
     title="Dislink bot",
     summary="Discord bot for creating link-shortcuts to servers.",
-    openapi_tags=json.loads(open("_public/tags_metadata.json", "r").read()), 
+    openapi_tags=json.loads(open("_locales/tags_metadata.json", "r").read()), 
     docs_url="/control/docs",
     redoc_url="/control/redocs",
     openapi_url="/control/openapi.json"
 )
 
-app.include_router(control_redirects.router)
-app.include_router(test_redirect.router)
+for part in routers:
+    app.include_router(part.router)
 
 if __name__ == '__main__':
     uvicorn.run("server:app", host='127.0.0.1', port=2525, reload=True)
