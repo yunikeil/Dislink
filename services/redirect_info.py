19d9cb128f2a670dd7735b79511deda511ac7e1f
yunikeil <iyunakov@inbox.ru>
2023-11-13 20:05:12 +0300

    ok, error in all responses json

--- services/redirect_info.py
+++ services/redirect_info.py
@@ -4,10 +4,10 @@ import sqlalchemy
 from sqlalchemy.orm import Session
 
 from db_models.redirect_info import RedirectInfo as RedirModelDB
-from dto.redirect_info import RedirectInfo as RedirModelDTO
+from dto.redirect_info import RedirectInfo as RedirInfoDTO
 
 
-def create_redirect(data: RedirModelDTO, db: Session):
+def create_redirect(data: RedirInfoDTO, db: Session):
     redirect = RedirModelDB(
         server_id=data.server_id,
         server_link=data.server_link,
@@ -45,20 +45,27 @@ def get_redirect(db: Session, server_id: int = None, domen_link: str = None):
         )
 
     else:
-        raise "any(server_id, domen_link) must be int, not NoneType"
+        return {"error": "any(server_id, domen_link) must be int, not NoneType"}
+
+    if not redirect:
+        return {"error": "There is no active redirection for this server"}  
 
     redirect.last_use = int(time.time())
     db.add(redirect)
     db.commit()
     db.refresh(redirect)
 
-    return redirect
+    return {"ok": redirect}
 
 
-def update_redirect(server_id: int, data: RedirModelDTO, db: Session):
+def update_redirect(data: RedirInfoDTO, db: Session):
     redirect = (
-        db.query(RedirModelDB).filter(RedirModelDB.server_id == server_id).first()
+        db.query(RedirModelDB).filter(RedirModelDB.server_id == data.server_id).first()
     )
+
+    if not redirect:
+        return {"error": "There is no active redirection for this server"}  
+
     redirect.server_link = data.server_link
     redirect.domen_link = data.domen_link
     redirect.last_use = int(time.time())
@@ -66,13 +73,17 @@ def update_redirect(server_id: int, data: RedirModelDTO, db: Session):
     db.commit()
     db.refresh(redirect)
 
-    return redirect
+    return {"ok": redirect}
 
 
 def remove_redirect(server_id: int, db: Session):
     redirect = (
         db.query(RedirModelDB).filter(RedirModelDB.server_id == server_id).delete()
     )
+
+    if not redirect:
+        return {"error": "There is no active redirection for this server"}  
+    
     db.commit()
 
-    return redirect
+    return {"ok": redirect}
