a14c2c8fcd69780a2e5b09b1a575db0a2facdfc1
yunikeil <iyunakov@inbox.ru>
2023-11-14 19:39:38 +0300

    bug fix

--- routers/usage_redirects.py
+++ routers/usage_redirects.py
@@ -30,8 +30,8 @@ async def redirector(request: Request, domen_link: str, db: Session = Depends(ge
     
     redirect_link = RedirictDB.get_redirect(db, domen_link=domen_link)
 
-    if text := redirect_link.get("ok").server_link:
-        return RedirectResponse(DISCORD_INVITE+text, status_code=301)
+    if data := redirect_link.get("ok"):
+        return RedirectResponse(DISCORD_INVITE+data.server_link, status_code=301)
     else:
         return JSONResponse({
             "detail": f"non-existent link: {str(request.base_url)+domen_link}",
