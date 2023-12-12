544d16485a6eb173a1fbbbf8227bf85949c0dd19
yunikeil <iyunakov@inbox.ru>
2023-12-12 21:02:00 +0300

    bug fix

--- app/redirect/schemas/redirect_info.py
+++ app/redirect/schemas/redirect_info.py
@@ -9,12 +9,11 @@ class RedirectCreate(BaseModel):
         min_length=2,
         max_length=30,
         pattern=r"^\D[а-яА-Яa-zA-Z0-9_-]+$",
-        default=None,
     )
 
 
 class RedirectUpdate(BaseModel):
-    server_link: str
+    server_link: str | None = None
     domen_link: str | None = Field(
         examples=["server"],
         min_length=2,
