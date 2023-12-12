c395dc7c3156a379f5ce62381a43ccc2b95ebb46
yunikeil <iyunakov@inbox.ru>
2023-12-12 23:04:34 +0300

    pre final version

--- cogs/redirect_manager.py
+++ cogs/redirect_manager.py
@@ -1,114 +1,228 @@
 import logging
-from requests.models import Response
+from typing import Tuple, Any
 from contextlib import asynccontextmanager
 
 import aiohttp
+from aiohttp import ClientResponse
 import discord
 from discord import app_commands
-from discord.ext import commands, tasks
+from discord.ext import commands
 from discord.app_commands import locale_str as _T
 
 
 @asynccontextmanager
 async def requests_error_handler(interaction: discord.Interaction):
+    errors = []  # Container to store errors
     try:
-        yield
+        yield errors
     except aiohttp.ClientConnectionError:
-        await interaction.response.send_message(_T("api_unavailable"))
-    except:
-        await interaction.response.send_message(_T("unknown_error"))
+        errors.append("api_unavailable")
+    except aiohttp.client_exceptions.ContentTypeError:
+        errors.append("api_unavailable")
+    except BaseException as e:
+        errors.append("unknown_error")
 
 
 class RedirectCog(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        print(type(self.create_shortcut))
-        self.on_init.start()
-
-    @tasks.loop(count=1)
-    async def on_init(self):
-        pass
     
+    @staticmethod
+    async def create_redirect(interaction, *, server_id, server_link, domen_link) -> Tuple[dict, Any]:
+        async with requests_error_handler(interaction) as errors:
+            url = f"https://dislink.space/c/redirect"
+            data = {"server_id": server_id, "server_link": server_link, "domen_link": domen_link}
+            async with aiohttp.ClientSession() as session:
+                async with session.post(url, json=data) as response:
+                    status = response.status
+                    response = await response.json()
+        return status, response, errors[0] if errors else None
+
+    @staticmethod
+    async def get_redirect(interaction, *, server_id) -> Tuple[dict, Any]:
+        async with requests_error_handler(interaction) as errors:
+            url = f"https://dislink.space/c/redirect"
+            params = {'server_id': server_id}
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url, params=params) as response:
+                    status = response.status
+                    response = await response.json()
+        return status, response, errors[0] if errors else None
+            
+    @staticmethod
+    async def update_redirect(interaction, *, server_id: int, server_link: str | None = None, domen_link: str | None = None) -> Tuple[dict, Any]:
+        async with requests_error_handler(interaction) as errors:
+            url = f"https://dislink.space/c/redirect"
+            params = {'server_id': server_id}
+            data = {"server_link": server_link, "domen_link": domen_link}
+            
+            if not data.get("server_link"):
+                del data["server_link"]
+            
+            if not data.get("domen_link"):
+                del data["domen_link"]
+            
+            async with aiohttp.ClientSession() as session:
+                async with session.put(url, json=data, params=params) as response:
+                    status = response.status
+                    response = await response.json()
+        return status, response, errors[0] if errors else None
+
+    @staticmethod
+    async def delete_redirect(interaction, *, server_id) -> Tuple[dict, Any]:
+        async with requests_error_handler(interaction) as errors:
+            url = f"https://dislink.space/c/redirect"
+            params = {'server_id': server_id}
+            async with aiohttp.ClientSession() as session:
+                async with session.delete(url, params=params) as response:
+                    status = response.status
+                    response = await response.json()
+        return status, response, errors[0] if errors else None
+        
     @app_commands.command(
         name=_T("create_shortcut_name"), description=_T("create_shortcut_description")
     )
-    @app_commands.guild_only
     @app_commands.default_permissions(administrator=True)
-    @app_commands.guilds(1169354124299603978)  # Temp decorator
-    @app_commands.rename(channel='channel')
-    @app_commands.rename(domen_link='domen_link')
-    @app_commands.describe(channel='channel_discr')
-    @app_commands.describe(domen_link='domen_link_discr')
+    @app_commands.guilds(1064192306904846377)
     async def create_shortcut(
         self,
         interaction: discord.Interaction,
         channel: discord.abc.GuildChannel,
         domen_link: str,
     ) -> None:
+        """
+        {'server_link': 'nK7QTt7bw9', 'domen_link': 'test', 'updated_at': 1702403522, 'server_id': 1064192306904846377, 'last_use': 1702403522, 'created_at': 1702403522}
+        """
         invite = await channel.create_invite(
             reason=f"{interaction.user.name} used /{_T('create_shortcut_name')}",
             max_age=0,
         )
-        async with requests_error_handler(interaction):
-            async with aiohttp.ClientSession() as session:
-                async with session.post(
-                    url=f"{api_url}/redirect",
-                    json={
-                        "server_id": interaction.guild_id,
-                        "server_link": invite.code,
-                        "domen_link": domen_link,
-                    },
-                ) as resp:
-                    response: dict = await resp.json()
-
-                    if resp.status == 200:
-                        await interaction.response.send_message(
-                            f"Link created: [dislink.space/{response.get('domen_link')}]"
-                            f"(https://discord.gg/{response.get('server_link')} )",
-                            ephemeral=True,
-                        )
-                    elif resp.status == 403:
-                        await interaction.response.send_message(
-                            f"{response.get('detail')}", ephemeral=True
-                        )
-                    else:
-                        await interaction.response.send_message(
-                            _T("unknown_error"), ephemeral=True
-                        )
-
+        status, response, server_error = await self.create_redirect(interaction, server_id=interaction.guild_id, server_link=invite.code, domen_link=domen_link)
+        
+        if server_error:
+            await interaction.response.send_message(
+                content=_T(server_error), ephemeral=True
+            )
+            return
+        
+        if not response:
+            await interaction.response.send_message(
+                content=_T("unknown_error"), ephemeral=True
+            )
+            return
+            
+        match status:
+            case 200:
+                # TODO embed response
+                await interaction.response.send_message(
+                    content=response
+                )
+            case 409:
+                await interaction.response.send_message(
+                    content=_T(response)
+                )
+            
     @app_commands.command(
         name=_T("get_shortcut_name"), description=_T("get_shortcut_description")
     )
     @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def get_shortcut(self, interaction: discord.Interaction) -> None:
-        async with requests_error_handler(interaction):
-            async with aiohttp.ClientSession() as session:  
-                ...
-
-        await interaction.response.send_message("Shortcut retrieved!", ephemeral=True)
-
+        status, response, server_error = await self.get_redirect(interaction, server_id=interaction.guild_id)
+        """
+        {'server_link': 'nK7QTt7bw9', 'domen_link': 'test', 'updated_at': 1702403522, 'server_id': 1064192306904846377, 'last_use': 1702403522, 'created_at': 1702403522}
+        """
+        if server_error:
+            await interaction.response.send_message(
+                content=_T(server_error), ephemeral=True
+            )
+            return
+        
+        if not response:
+            await interaction.response.send_message(
+                content=_T("unknown_error"), ephemeral=True
+            )
+            return
+            
+        match status:
+            case 200:
+                # TODO embed response
+                await interaction.response.send_message(
+                    content=response
+                )
+            case 404:
+                await interaction.response.send_message(
+                    content=_T(response)
+                )
+    
     @app_commands.command(
         name=_T("update_shortcut_name"), description=_T("update_shortcut_description")
     )
     @app_commands.guilds(1064192306904846377)  # Temp decorator
-    async def update_shortcut(self, interaction: discord.Interaction) -> None:
-        async with requests_error_handler(interaction):
-            async with aiohttp.ClientSession() as session:
-                ...
-
-        await interaction.response.send_message("Shortcut updated!", ephemeral=True)
-
+    async def update_shortcut(self, interaction: discord.Interaction, domen_link: str) -> None:
+        status, response, server_error = await self.update_redirect(interaction, server_id=interaction.guild_id, domen_link=domen_link)
+        
+        if server_error:
+            await interaction.response.send_message(
+                content=_T(server_error), ephemeral=True
+            )
+            return
+        
+        if not response:
+            await interaction.response.send_message(
+                content=_T("unknown_error"), ephemeral=True
+            )
+            return
+            
+        match status:
+            case 200:
+                # TODO embed response
+                await interaction.response.send_message(
+                    content=response
+                )
+            case 404:
+                await interaction.response.send_message(
+                    content=_T(response)
+                )
+            case 409:
+                await interaction.response.send_message(
+                    content=_T(response)
+                )    
+    
     @app_commands.command(
         name=_T("delete_shortcut_name"), description=_T("delete_shortcut_description")
     )
     @app_commands.guilds(1064192306904846377)  # Temp decorator
     async def delete_shortcut(self, interaction: discord.Interaction) -> None:
-        async with requests_error_handler(interaction):
-            async with aiohttp.ClientSession() as session:
-                ...
-
-        await interaction.response.send_message("Shortcut deleted!", ephemeral=True)
-
+        # TODO удаление приглашения
+        status, response, server_error = await self.delete_redirect(interaction, server_id=interaction.guild_id)
+        
+        if server_error:
+            await interaction.response.send_message(
+                content=_T(server_error), ephemeral=True
+            )
+            return
+        
+        if not response:
+            await interaction.response.send_message(
+                content=_T("unknown_error"), ephemeral=True
+            )
+            return
+            
+        match status:
+            case 200:
+                # TODO embed response
+                await interaction.response.send_message(
+                    content=response
+                )
+            case 404:
+                await interaction.response.send_message(
+                    content=_T(response)
+                )
+    
+    @commands.Cog.listener()
+    async def on_invite_delete(self, invite: discord.Invite):
+        # TODO Удаление записи на сервере
+        print(f'Приглашение {invite.code} было удалено.')
 
 async def setup(bot: commands.Bot) -> None:
     logging.getLogger("discord.cogs.load").info("RedirectCog loaded!")
